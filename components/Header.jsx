fb2286053996d6925918c6255f22b5d4d00e5631
Prasad Nageshkar <prasad.nageshkar@gmail.com>
2022-09-07 19:18:19 +0530

    Styled the header section

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import HeaderIcon from './HeaderIcon';
+import {HomeIcon,UserIcon,PhoneIcon,InformationCircleIcon } from '@heroicons/react/24/solid';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className= 'bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between'>
+        <div className='flex gap-6'>
+            <HeaderIcon Icon={HomeIcon} title ="HOME"/>
+            <HeaderIcon Icon={UserIcon} title ="ACCOUNT"/>
+            <HeaderIcon Icon={PhoneIcon} title ="CONTACT"/>
+            <HeaderIcon Icon={InformationCircleIcon}  title ="ABOUT"/>
+        </div>
+        <Image 
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/1200px-IMDB_Logo_2016.svg.png" 
+            width={100}
+            height={50}
+            className="cursor-pointer active:brightness-110"
+        />
+    </div>
   )
 }
